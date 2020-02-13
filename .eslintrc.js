bb01c7530fb4cb881e87a9712bfa58837868a12b
Alberto Matias <alberto.matias@bbva.com>
2020-02-13 16:29:29 +0100

    chore(ember): update to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,58 @@
 'use-strinct';
 
 module.exports = {
-	extends: 'eslint-config-bbva',
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		ecmaFeatures: {
+			legacyDecorators: true
+		}
+	},
+	plugins: [
+		'ember'
+	],
+	extends: [
+		'plugin:ember/recommended',
+		'eslint-config-bbva'
+	],
 	env: {
 		browser: true
-	}
+	},
+	rules: {
+		'ember/no-jquery': 'error'
+	},
+	overrides: [
+		// node files
+		{
+			files: [
+				'.eslintrc.js',
+				'.template-lintrc.js',
+				'ember-cli-build.js',
+				'index.js',
+				'testem.js',
+				'blueprints/*/index.js',
+				'config/**/*.js',
+				'tests/dummy/config/**/*.js'
+			],
+			excludedFiles: [
+				'addon/**',
+				'addon-test-support/**',
+				'app/**',
+				'tests/dummy/app/**'
+			],
+			parserOptions: {
+				sourceType: 'script'
+			},
+			env: {
+				browser: false,
+				node: true
+			},
+			plugins: ['node'],
+			rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+				// add your custom rules and overrides for node files here
+			})
+		}
+	]
 };
